60e917badfc5ca2f6cc3a362607d0c6fa1ce9fbf
David Myers <34919054+dmyers2020@users.noreply.github.com>
2020-02-05 22:49:24 -0800

    added a few files
    get started is just a few reminders about the python dev workflow. 
    excelscrape_try.py is the first few read/wrangle lines
    and the example doc is just some random gibberish to practice wrangling.

---
+++ excelscrape_try.py
@@ -0,0 +1,15 @@
+# Readme
+# Environment
+# Requirements
+	pip (to install pandas) 
+		pandas as pd
+		xlrd
+	
+
+# Dependancies
+
+a =pd.read_excel('samplebook.xlsx', sheet_name=1)
+df=pd.DataFrame(a)
+df.c1
+df.drop(columns=['
+df=df.dropna(axis=1,thresh=10) #drop columns containing'thresh' (10) nulls
